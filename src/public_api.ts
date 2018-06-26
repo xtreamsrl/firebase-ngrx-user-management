a7c006772742d0a7fe7de0fc9076367a22504fe7
Simone Colucci <simone.colucci@xtreamers.io>
2018-06-26 11:31:04 +0200

    Change export policy

--- src/public_api.ts
+++ src/public_api.ts
@@ -2,10 +2,12 @@
  * Public API Surface of firebase-ngrx-user-management
  */
 
-import * as ua from './lib/actions/auth.actions';
 import {IUser, User} from './lib/models/auth.model';
 
 export * from './lib/firebase-ngrx-user-management.service';
 export * from './lib/firebase-ngrx-user-management.module';
-const userActions = ua;
-export {userActions, IUser, User};
+export * from './lib/actions/auth.actions';
+
+import {AuthState} from './lib/reducers/user.reducer';
+
+export {AuthState, IUser, User};
